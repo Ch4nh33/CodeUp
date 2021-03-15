f3f7ce9bb6bb87c1a23754a09be63e1b89c33343
Ch4nh33 <78791504+Ch4nh33@users.noreply.github.com>
2021-03-16 05:42:55 +0900

    Create 1147 : 비트 연산자(<<).c

---
+++ 1147 : 비트 연산자(<<).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int bit_shift(int a, int b)
+{
+    return a << b;
+}
+int main(void)
+{
+    int n,m;
+    scanf("%d %d",&n,&m);
+    printf("%d",bit_shift(n,m));
+    return 0;
+}
