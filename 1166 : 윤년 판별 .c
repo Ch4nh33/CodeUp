8f711c02733b28ecc355f11d3d1733018a64163f
Ch4nh33 <78791504+Ch4nh33@users.noreply.github.com>
2021-03-16 21:53:19 +0900

    Create 1166 : 윤년 판별 .c

---
+++ 1166 : 윤년 판별 .c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+  int year;
+  scanf("%d",&year);
+  if((year%4==0 && year%100!=0)||(year%400==0))
+    printf("yes");
+  else  
+    printf("no");
+  return 0;
+}
